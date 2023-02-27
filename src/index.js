24a51d6823babd9bcc7d756586c8c6bf297c7d90
Abylkhayir <88201428+Abylkhayir@users.noreply.github.com>
2023-02-28 00:20:22 +0600

    feat:update this file

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let morse = '';
+    let res = '';
+    let arr = expr.split('');
+
+    for(let i = 0; i<arr.length; i++) {
+        if(arr[i] == ' ') {
+            res += '**********';
+            continue;
+        }
+        for(key in MORSE_TABLE) {
+            if(MORSE_TABLE[key] == arr[i]){
+                morse += key;
+
+                let numArr = morse.split('').map(e => e === '-' ? '11' : e === '.' ? '10' : null).join('')
+
+                if(numArr.length < 10){
+                    let a = 10 - numArr.length;
+
+                    for(let k = 0; k<a; k++){
+                        numArr = '0' + numArr;
+                    }
+                
+                }
+
+                res += numArr;
+                numArr = '';
+                morse = '';
+                
+            }
+        }
+    }
+
+    
+
+    return res;
 }
 
 module.exports = {
